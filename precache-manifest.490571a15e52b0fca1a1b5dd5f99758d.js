b0d93478d643a6ed6151596239d17a2892d8cb55
novatriansyah <novaajis20@gmail.com>
2020-03-05 13:13:35 +0700

    Updates

---
+++ precache-manifest.490571a15e52b0fca1a1b5dd5f99758d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be8062ab478db8d5cf061520320acbb",
+    "url": "/Portfolio-Nova-Azis/index.html"
+  },
+  {
+    "revision": "fbcb38ad0e00c13d1c43",
+    "url": "/Portfolio-Nova-Azis/static/js/2.55439bb2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Portfolio-Nova-Azis/static/js/2.55439bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6309ff2dd2ccf84db023",
+    "url": "/Portfolio-Nova-Azis/static/js/main.cf1bbfe8.chunk.js"
+  },
+  {
+    "revision": "14c006c5d84d4382e881",
+    "url": "/Portfolio-Nova-Azis/static/js/runtime-main.df18285a.js"
+  }
+]);
